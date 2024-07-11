0be86e61ddbb6a9379288a03a5c9615ade69d112
Ewan Miller <ewan.miller@kcl.ac.uk>
2024-07-11 18:28:33 -0400

    make constants file so we can be consistent across the project... it's a bit empty at the moment

---
+++ nuTens/propagator/constants.hpp
@@ -0,0 +1,9 @@
+
+/// @file constant.hpp
+
+//! Define constants to be used across the project
+namespace Constants{
+
+    static constexpr float Groot2 = 1.52588e-4; //!< sqrt(2) G_fermi used in calculating matter hamiltonian
+
+}
