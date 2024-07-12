1c3b443cf50a1085f602d89786d820c179d0ec6a
Ewan Miller <ewan.miller@kcl.ac.uk>
2024-07-12 06:31:30 -0400

    add a barger propagator class to be used when testing so that we have something to compare results to

---
+++ tests/test-utils.hpp
@@ -0,0 +1,9 @@
+#include <math.h>
+
+// Some helpful utility functions for testing
+
+// Get absolute relative difference between two floats:
+//   | (f1 - f2) / f1 |
+float relativeDiff(float f1, float f2){
+    return std::abs((f1 - f2) / f1);
+}
