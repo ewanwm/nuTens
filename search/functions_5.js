2b5ff5c05cccf99dc36e22a5cf99f3d489a61b44
ewanwm <ewan.miller@kcl.ac.uk>
2025-07-07 13:36:20 +0000

    Deploying to gh-pages from @ ewanwm/nuTens@7e01f2112dae862f7d7f35c0355c9bf0e7d1831a 🚀

--- search/functions_5.js
+++ search/functions_5.js
@@ -1,10 +1,11 @@
 var searchData=
 [
   ['getbatchdim_0',['getBatchDim',['../classTensor.html#a784131f37c10b8a801a053d1044aec81',1,'Tensor']]],
-  ['getndim_1',['getNdim',['../classTensor.html#afbb1cf75cc2d97ac21ee63f4c7482eee',1,'Tensor']]],
-  ['getshape_2',['getShape',['../classTensor.html#aacded5eb407c8e510f09dd3be3c83025',1,'Tensor']]],
-  ['gettensorlibrary_3',['getTensorLibrary',['../classTensor.html#a4769ffbca778565253f564b1a8970dfd',1,'Tensor']]],
-  ['getvalues_4',['getValues',['../classTensor.html#afd77ec799413df091b28a32a9ca9fcf5',1,'Tensor']]],
-  ['getvariantvalue_5',['getVariantValue',['../classTensor.html#a7b16fca56a508a204d9f090a90fade3e',1,'Tensor']]],
-  ['grad_6',['grad',['../classTensor.html#a895f50b8abeff5f234488397c7db09ab',1,'Tensor']]]
+  ['gethasbatchdim_1',['getHasBatchDim',['../classTensor.html#af47215396e4ed581802772f9e53f7ddf',1,'Tensor']]],
+  ['getndim_2',['getNdim',['../classTensor.html#afbb1cf75cc2d97ac21ee63f4c7482eee',1,'Tensor']]],
+  ['getshape_3',['getShape',['../classTensor.html#aacded5eb407c8e510f09dd3be3c83025',1,'Tensor']]],
+  ['gettensorlibrary_4',['getTensorLibrary',['../classTensor.html#a4769ffbca778565253f564b1a8970dfd',1,'Tensor']]],
+  ['getvalues_5',['getValues',['../classTensor.html#afd77ec799413df091b28a32a9ca9fcf5',1,'Tensor']]],
+  ['getvariantvalue_6',['getVariantValue',['../classTensor.html#a7b16fca56a508a204d9f090a90fade3e',1,'Tensor']]],
+  ['grad_7',['grad',['../classTensor.html#a895f50b8abeff5f234488397c7db09ab',1,'Tensor']]]
 ];
