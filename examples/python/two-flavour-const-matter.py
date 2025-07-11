2b5e47c2e9e1ee102101f4861a4516f908692531
Ewan Miller <ewan.miller@kcl.ac.uk>
2025-07-11 17:56:05 +0200

    use new units in python examples

--- examples/python/two-flavour-const-matter.py
+++ examples/python/two-flavour-const-matter.py
@@ -4,7 +4,7 @@ from nuTens.tensor import Tensor
 import matplotlib.pyplot as plt
 import typing
 
-N_ENERGIES = 1000
+N_ENERGIES = 10000
 
 def build_PMNS(theta12: Tensor):
     """ Construct a PMNS matrix in the usual parameterisation """
@@ -22,12 +22,12 @@ def build_PMNS(theta12: Tensor):
 energies = nt.tensor.ones([N_ENERGIES, 1], nt.dtype.scalar_type.complex_float, nt.dtype.device_type.cpu, False)
 
 for i in range(N_ENERGIES):
-    energies.set_value([i,0], 1.0 + i*0.2)
+    energies.set_value([i,0], ( 1.0e-6 + i*0.2e-3 ) * nt.units.GeV)
 
 energies.requires_grad(True)
 
 ## define tensors with oscillation parameters
-theta12 = Tensor([0.58], nt.dtype.scalar_type.complex_float, nt.dtype.device_type.cpu, True)
+theta12 = Tensor([0.15], nt.dtype.scalar_type.complex_float, nt.dtype.device_type.cpu, True)
 
 ## make the matrix
 PMNS = build_PMNS(theta12)
@@ -35,8 +35,8 @@ PMNS = build_PMNS(theta12)
 ## set the mass tensor
 masses = nt.tensor.zeros([1,2], nt.dtype.scalar_type.float, nt.dtype.device_type.cpu, True)
 
-masses.set_value([0,0], 0.0)
-masses.set_value([0,1], 0.00868e-3)
+masses.set_value([0,0], 0.00868 * nt.units.eV)
+masses.set_value([0,1], 0.0501 * nt.units.eV)
 
 ## print info about the parameters
 print("PMNS: ")
@@ -47,7 +47,7 @@ print(masses.to_string())
 print()
 
 ## set up the propagator object
-propagator = nt.propagator.Propagator(2, 10) #23.79e12)#295000.0)
+propagator = nt.propagator.Propagator(2, 295 * nt.units.km)
 matter_solver = nt.propagator.ConstDensitySolver(2, 2.79)
 
 propagator.set_PMNS(PMNS)
@@ -94,19 +94,17 @@ for i in range(N_ENERGIES):
         probabilities.get_value([i, 1, 1]) 
     )
 
-plt.plot(energy_list, e_survival_prob_list, label = "electron")
-plt.plot(energy_list, mu_survival_prob_list, label = "muon")
-plt.xlabel("Energy [MeV]")
+plt.plot([ e / nt.units.GeV for e in energy_list ], e_survival_prob_list, label = "a -> a")
+plt.plot([ e / nt.units.GeV for e in energy_list ], mu_survival_prob_list, label = "b -> b")
+plt.xlabel("Energy [GeV]")
 plt.ylabel("Survival probability")
 plt.legend()
 plt.show()
 plt.savefig("two-flavour-matter-survival-probs.png")
 
 plt.clf()
-plt.plot(energy_list, mu_to_e_prob_list, label = "numu -> nue")
-plt.plot(energy_list, mu_survival_prob_list, label = "numu -> numu")
-plt.plot(energy_list, mu_total_prob_list, label = "Total")
-plt.xlabel("Energy [MeV]")
+plt.plot([ e / nt.units.GeV for e in energy_list ], mu_to_e_prob_list, label = "b -> a")
+plt.xlabel("Energy [GeV]")
 plt.ylabel("Oscillation probability")
 plt.legend()
 plt.show()
