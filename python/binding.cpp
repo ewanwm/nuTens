46f23c05f0b92fef75f9a3fa0642d735b5da4999
Ewan Miller <ewan.miller@kcl.ac.uk>
2025-07-12 18:51:57 +0200

    more docstringing

--- python/binding.cpp
+++ python/binding.cpp
@@ -49,30 +49,52 @@ void initTensor(py::module &m)
         .def(py::init<std::vector<float>, NTdtypes::scalarType, NTdtypes::deviceType, bool>())
 
         // property setters
-        .def("dtype", &Tensor::dType, py::return_value_policy::reference, "Set the data type of the tensor")
-        .def("device", &Tensor::device, py::return_value_policy::reference, "Set the device that the tensor lives on")
+        .def("dtype", &Tensor::dType, py::return_value_policy::reference, 
+            "Set the data type of the tensor",
+            py::arg("new_dtype")
+        )
+        .def("device", &Tensor::device, py::return_value_policy::reference, 
+            "Set the device that the tensor lives on",
+            py::arg("new_device")
+        )
         .def("requires_grad", &Tensor::requiresGrad, py::return_value_policy::reference,
-            "Set Whether or not this tensor requires gradient to be calculated")
+            "Set Whether or not this tensor requires gradient to be calculated",
+            py::arg("new_value")
+        )
         .def("has_batch_dim", &Tensor::getHasBatchDim,
-            "Check Whether or not the first dimension should be interpreted as a batch dim for this tensor")
+            "Check Whether or not the first dimension should be interpreted as a batch dim for this tensor"
+        )
         .def("has_batch_dim", &Tensor::hasBatchDim, py::return_value_policy::reference,
-            "Set Whether or not the first dimension should be interpreted as a batch dim for this tensor")
+            "Set Whether or not the first dimension should be interpreted as a batch dim for this tensor",
+            py::arg("new_value")
+        )
 
         // utilities
-        .def("to_string", &Tensor::toString, "print out a summary of this tensor to a string")
+        .def("to_string", &Tensor::toString, 
+            "get a summary of this tensor as a string"
+        )
         .def("add_batch_dim", &Tensor::addBatchDim, py::return_value_policy::reference,
-            "Add a batch dimension to the start of this tensor if it doesn't have one already")
+            "Add a batch dimension to the start of this tensor if it doesn't have one already"
+        )
 
         // setters
         .def("set_value", py::overload_cast<const Tensor &, const Tensor &>(&Tensor::setValue),
-            "Set a value at a specific index of this tensor")
+            "Set a value at a specific index of this tensor",
+            py::arg("indices"), py::arg("value")
+        )
         .def("set_value",
             py::overload_cast<const std::vector<std::variant<int, std::string>> &, const Tensor &>(&Tensor::setValue),
-            "Set a value at a specific index of this tensor")
+            "Set a value at a specific index of this tensor",
+            py::arg("indices"), py::arg("value")
+        )
         .def("set_value", py::overload_cast<const std::vector<int> &, float>(&Tensor::setValue),
-            "Set a value at a specific index of this tensor")
+            "Set a value at a specific index of this tensor",
+            py::arg("indices"), py::arg("value")
+        )
         .def("set_value", py::overload_cast<const std::vector<int> &, std::complex<float>>(&Tensor::setValue),
-            "Set a value at a specific index of this tensor")
+            "Set a value at a specific index of this tensor",
+            py::arg("indices"), py::arg("value")
+        )
 
         // getters
         .def("get_shape", &Tensor::getShape, "Get the shape of this tensor")
@@ -112,27 +134,90 @@ void initTensor(py::module &m)
         .def_static("zeros", &Tensor::zeros, 
             "Create a tensor initialised with zeros",
             py::arg("shape"), py::arg("dtype") = NTdtypes::kFloat, py::arg("device") = NTdtypes::kCPU, py::arg("requires_grad") = true)
+
+        .doc() = 
+            "Tensor defines a basic interface for creating and manipulating tensors."
+            "To create tensors you should use the static constructor methods.\n"
+            "Alternatively you can chain together multiple property setters.\n"
+            "\n"
+            "For example\n"
+            "\n"
+            ".. code-block::\n"
+            "\n"    
+            "    from nuTens.tensor import Tensor, dtype\n"  
+            "    tensor = Tensor.ones([3,3], dtype.scalar_type.float, dtype.device_type.cpu)\n"
+            "\n"
+            "will get you a 3x3 tensor of floats that lives on the CPU.\n"
+            "\n"
+            "This is equivalent to\n"
+            "\n"
+            ".. code-block::"
+            "\n"
+            "    tensor = Tensor.ones([3,3]).dtype(dtype.scalar_type.float).device(dtype.device_type.cpu);\n"
+            "\n"
     ;
 
     // maffs
-    m_tensor.def("matmul", &Tensor::matmul, "Matrix multiplication");
-    m_tensor.def("outer", &Tensor::outer, "Tensor outer product");
-    m_tensor.def("mul", &Tensor::mul, "Element-wise multiplication");
-    m_tensor.def("div", &Tensor::div, "Element-wise division");
-    m_tensor.def("pow", py::overload_cast<const Tensor &, float>(&Tensor::pow), "Raise to scalar power");
-    m_tensor.def("pow", py::overload_cast<const Tensor &, std::complex<float>>(&Tensor::pow), "Raise to scalar power");
-    m_tensor.def("exp", &Tensor::exp, "Take exponential");
-    m_tensor.def("transpose", &Tensor::transpose, "Get the matrix transpose");
-    m_tensor.def("scale", py::overload_cast<const Tensor &, float>(&Tensor::scale), "Scalar multiplication");
+    m_tensor.def("matmul", &Tensor::matmul, 
+        "Matrix multiplication",
+        py::arg("t1"), py::arg("t2")
+    );
+    m_tensor.def("outer", &Tensor::outer, 
+        "Tensor outer product",
+        py::arg("t1"), py::arg("t2")
+    );
+    m_tensor.def("mul", &Tensor::mul, 
+        "Element-wise multiplication",
+        py::arg("t1"), py::arg("t2")
+    );
+    m_tensor.def("div", &Tensor::div, 
+        "Element-wise division",
+        py::arg("t1"), py::arg("t2")
+    );
+    m_tensor.def("pow", py::overload_cast<const Tensor &, float>(&Tensor::pow), 
+        "Raise to scalar power",
+        py::arg("t1"), py::arg("power")
+    );
+    m_tensor.def("pow", py::overload_cast<const Tensor &, std::complex<float>>(&Tensor::pow), 
+        "Raise to scalar power",
+        py::arg("t1"), py::arg("power")
+    );
+    m_tensor.def("exp", &Tensor::exp, 
+        "Take element-wise exponential of a tensor",
+        py::arg("t1")
+    );
+    m_tensor.def("transpose", &Tensor::transpose, 
+        "Get the matrix transpose",
+        py::arg("t1"), py::arg("index_1"), py::arg("index_2")
+    );
+    m_tensor.def("scale", py::overload_cast<const Tensor &, float>(&Tensor::scale), 
+        "Scalar multiplication",
+        py::arg("t1"), py::arg("scalar")
+    );
     m_tensor.def("scale", py::overload_cast<const Tensor &, std::complex<float>>(&Tensor::scale),
-                "Scalar multiplication");
-    m_tensor.def("sin", &Tensor::sin, "Element-wise trigonometric sine function");
-    m_tensor.def("cos", &Tensor::cos, "Element-wise trigonometric cosine function");
-    m_tensor.def("sum", py::overload_cast<const Tensor &>(&Tensor::sum), "Get the sum of all values in a tensor");
+        "Scalar multiplication",
+        py::arg("t1"), py::arg("scalar")
+    );
+    m_tensor.def("sin", &Tensor::sin, 
+        "Element-wise trigonometric sine function",
+        py::arg("t1")
+    );
+    m_tensor.def("cos", &Tensor::cos, 
+        "Element-wise trigonometric cosine function",
+        py::arg("t1")
+    );
+    m_tensor.def("sum", py::overload_cast<const Tensor &>(&Tensor::sum), 
+        "Get the sum of all values in a tensor",
+        py::arg("t1")
+    );
     m_tensor.def("sum", py::overload_cast<const Tensor &, const std::vector<long int> &>(&Tensor::sum),
-                "Get the sum of all values in a tensor");
+        "Get the sum over particular dimensions",
+        py::arg("t1"), py::arg("dimensions")
+    );
     m_tensor.def("cumsum", py::overload_cast<const Tensor &, int>(&Tensor::cumsum),
-                "Get the cumulative sum over some dimension");
+        "Get the cumulative sum over particular dimensions",
+        py::arg("t1"), py::arg("dimensions")
+    );
     // m_tensor.def("eig", &Tensor::eig. "calculate eigenvalues") <- Will need to define some additional fn to return
     // tuple of values
 }
@@ -144,28 +229,50 @@ void initPropagator(py::module &m)
     py::class_<Propagator>(m_propagator, "Propagator")
         .def(py::init<int, float>())
         .def("calculate_probabilities", &Propagator::calculateProbs,
-            "Calculate the oscillation probabilities for neutrinos of specified energies")
+            "Calculate the oscillation probabilities for neutrinos of specified energies"
+        )
         .def("set_matter_solver", &Propagator::setMatterSolver,
-            "Set the matter effect solver that the propagator should use")
-        .def("set_masses", &Propagator::setMasses, "Set the neutrino mass state eigenvalues")
+            "Set the matter effect solver that the propagator should use",
+            py::arg("new_matter_solver")
+        )
+        .def("set_masses", &Propagator::setMasses, 
+            "Set the neutrino mass state eigenvalues",
+            py::arg("new_masses")
+        )
         .def("set_energies", py::overload_cast<Tensor &>(&Propagator::setEnergies),
-            "Set the neutrino energies that the propagator should use")
+            "Set the neutrino energies that the propagator should use",
+            py::arg("new_energies")
+        )
         .def("set_PMNS", py::overload_cast<Tensor &>(&Propagator::setPMNS),
-            "Set the PMNS matrix that the propagator should use")
+            "Set the PMNS matrix that the propagator should use",
+            py::arg("new_matrix")
+        )
         .def("set_PMNS", py::overload_cast<const std::vector<int> &, float>(&Propagator::setPMNS),
-            "Set the PMNS matrix that the propagator should use")
+            "Set a particular value within the PMNS matrix used by the propagator",
+            py::arg("indices"), py::arg("value")
+        )
         .def("set_PMNS", py::overload_cast<const std::vector<int> &, std::complex<float>>(&Propagator::setPMNS),
-            "Set the PMNS matrix that the propagator should use");
+            "Set the PMNS matrix that the propagator should use",
+            py::arg("indices"), py::arg("value")
+        );
 
-    py::class_<BaseMatterSolver, std::shared_ptr<BaseMatterSolver>>(m_propagator, "BaseSolver")
+    py::class_<BaseMatterSolver, std::shared_ptr<BaseMatterSolver>>(m_propagator, "BaseMatterSolver")
         .def("set_PMNS", &BaseMatterSolver::setPMNS,
-            "Set the PMNS matrix that the solver should use")
+            "Set the PMNS matrix that the solver should use",
+            py::arg("new_matrix")
+        )
         .def("set_energies", &BaseMatterSolver::setEnergies,
-            "Set the neutrino energies")
+            "Set the neutrino energies",
+            py::arg("new_energies")
+        )
         .def("set_masses", &BaseMatterSolver::setMasses,
-            "Set the neutrino masses the solver should use")
+            "Set the neutrino masses the solver should use",
+            py::arg("new_masses")
+        )
         .def("calculate_eigenvalues", &BaseMatterSolver::calculateEigenvalues,
-            "calculate the eigenvalues of the Hamiltonian")
+            "calculate the eigenvalues of the Hamiltonian",
+            py::arg("eigenvector_out"), py::arg("eigenvalue_out")
+        )
         ;
 
      py::class_<ConstDensityMatterSolver, std::shared_ptr<ConstDensityMatterSolver>, BaseMatterSolver>(
@@ -175,7 +282,8 @@ void initPropagator(py::module &m)
 
 void initDtypes(py::module &m)
 {
-    auto m_dtypes = m.def_submodule("dtype");
+    auto m_dtypes = m.def_submodule("dtype",
+        "This module defines various data types used in nuTens");
 
     py::enum_<NTdtypes::scalarType>(m_dtypes, "scalar_type")
         .value("int", NTdtypes::scalarType::kInt)
@@ -193,7 +301,8 @@ void initDtypes(py::module &m)
 
 void initUnits(py::module &m)
 {
-    auto m_units = m.def_submodule("units");
+    auto m_units = m.def_submodule("units",
+        "Defines some helpful units, which are really just conversion factors to eV");
 
     m_units.attr("eV")  = py::float_(Units::eV);
     m_units.attr("MeV") = py::float_(Units::MeV);
@@ -207,7 +316,8 @@ void initUnits(py::module &m)
 
 void initTesting(py::module &m)
 {
-    auto m_testing = m.def_submodule("testing");
+    auto m_testing = m.def_submodule("testing",
+        "Some helpful utilities to use when writing python tests for your code");
 
     py::class_<Testing::TwoFlavourBarger>(m_testing, "TwoFlavourBarger")
         .def(py::init<>())
