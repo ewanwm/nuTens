6384528233d208d96c42a16f5f825b9a63e82ae5
Ewan Miller <ewan.miller@kcl.ac.uk>
2025-07-23 01:04:37 +0200

    start on using gtest in tensor test

--- tests/test-tensor-basic.cpp
+++ tests/test-tensor-basic.cpp
@@ -2,12 +2,68 @@
 #include <nuTens/tensors/dtypes.hpp>
 #include <nuTens/tensors/tensor.hpp>
 
+#include <gtest/gtest.h>
+
 /*
     Do some very basic tests of tensor functionality
     e.g. test that complex matrices work as expected, 1+1 == 2 etc.
 */
 
 using namespace nuTens;
+
+// check creation of tensors
+TEST(Tensor, TensorCreationFloat) {
+
+    Tensor zero = Tensor::zeros({1}, dtypes::kFloat, dtypes::kCPU, false);
+    ASSERT_EQ(zero.getValue<float>(), 0.0);
+
+    Tensor one = Tensor::ones({1}, dtypes::kFloat, dtypes::kCPU, false);
+    ASSERT_EQ(one.getValue<float>(), 1.0);
+
+    Tensor three = Tensor({3.0}, dtypes::kFloat, dtypes::kCPU, false);
+    ASSERT_EQ(three.getValue<float>(), 3.0);
+
+    Tensor diagonal = Tensor({0.0, 1.0, 2.0}, dtypes::kFloat, dtypes::kCPU, false);
+    Tensor diagTensor = Tensor::diag(diagonal);
+    ASSERT_EQ(diagTensor.getValue<float>({0}), 0.0);
+    ASSERT_EQ(diagTensor.getValue<float>({1}), 1.0);
+    ASSERT_EQ(diagTensor.getValue<float>({2}), 2.0);
+
+    Tensor eye = Tensor::eye(2, dtypes::kFloat, dtypes::kCPU, false);
+    ASSERT_EQ(eye.getValue<float>({0,0}), 1.0);
+    ASSERT_EQ(eye.getValue<float>({1,1}), 1.0);
+    ASSERT_EQ(eye.getValue<float>({0,1}), 0.0);
+    ASSERT_EQ(eye.getValue<float>({1,0}), 0.0);
+}
+
+
+// check some basic arithmetic
+TEST(Tensor, simpleArithmeticFloat) {
+
+    Tensor one = Tensor::ones({1}, dtypes::kFloat, dtypes::kCPU, false);
+    ASSERT_EQ((one + one).getValue<float>(), 2.0);
+    ASSERT_EQ((one - one).getValue<float>(), 0.0);
+
+    Tensor ten  = Tensor({10.0}, dtypes::kFloat, dtypes::kCPU, false);
+    Tensor five = Tensor({5.0}, dtypes::kFloat, dtypes::kCPU, false);
+    ASSERT_EQ(Tensor::div(ten, five).getValue<float>(), 2.0);
+    ASSERT_EQ(Tensor::mul(ten, five).getValue<float>(), 50.0);
+    ASSERT_EQ(Tensor::pow(ten, 2.0).getValue<float>(), 100.0);
+
+    Tensor four = Tensor({4.0}, dtypes::kFloat, dtypes::kCPU, false);
+    ASSERT_EQ(Tensor::pow(ten, 0.5).getValue<float>(), 2.0);
+
+    ASSERT_EQ(Tensor::scale(one, 1.234).getValue<float>(), 1.234);
+}
+
+// check some basic arithmetic
+TEST(Tensor, simpleArithmeticComplexFloat) {
+
+    Tensor one = Tensor::ones({1}, dtypes::kComplexFloat, dtypes::kCPU, false);
+    ASSERT_EQ((one + one).getValue<std::complex<float>>(), std::complex<float>(2.0, 0.0));
+    ASSERT_EQ((one - one).getValue<std::complex<float>>(), std::complex<float>(0.0, 0.0));
+
+}
 
 int main()
 {
