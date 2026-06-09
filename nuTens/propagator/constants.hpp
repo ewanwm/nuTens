1d6e9a635e8a6a297effc8be0651d025bda7ef6a
Ewan W Miller <e.w.miller@imperial.ac.uk>
2026-06-10 00:54:49 +0100

    make 2 pi a constant

--- nuTens/propagator/constants.hpp
+++ nuTens/propagator/constants.hpp
@@ -4,6 +4,7 @@
 /// @brief Defines constants to be used across the project
 
 #include <nuTens/propagator/units.hpp>
+#include <math.h>
 
 namespace nuTens::constants
 {
@@ -11,5 +12,7 @@ namespace nuTens::constants
 static constexpr double Groot2 =
     0.76294e-4 * (units::eV * units::eV) /
     units::GeV; //!< sqrt(2)*G_fermi in (eV^2-cm^3)/(mole-GeV) used in calculating matter hamiltonian
+
+static constexpr double twoPi = 2.0 * M_PI;
 
 } // namespace nuTens::constants
