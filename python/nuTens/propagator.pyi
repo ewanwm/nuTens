06a1bfa8f55a3659d3780cbe6e4b47c7f4c3b746
ewanwm <e.w.miller@imperial.ac.uk>
2026-06-15 14:55:52 +0000

    Commit python stubs (from Github Action: "Generate Python Stubs")

--- python/nuTens/propagator.pyi
+++ python/nuTens/propagator.pyi
@@ -1,5 +1,4 @@
 from __future__ import annotations
-import collections.abc
 import nuTens._pyNuTens.tensor
 import typing
 __all__: list[str] = ['BaseMatterSolver', 'BaseMixingMatrix', 'ConstDensitySolver', 'DPpropagator', 'PMNSmatrix', 'Propagator']
@@ -8,19 +7,19 @@ class BaseMatterSolver:
         """
         calculate the eigenvalues of the Hamiltonian
         """
-    def set_antineutrino(self, new_value: bool) -> None:
+    def set_antineutrino(self, new_value: bool) -> BaseMatterSolver:
         """
         Set whether the solver should calculate values for anti-neutrinos
         """
-    def set_energies(self, new_energies: nuTens._pyNuTens.tensor.Tensor) -> None:
+    def set_energies(self, new_energies: nuTens._pyNuTens.tensor.Tensor) -> BaseMatterSolver:
         """
         Set the neutrino energies
         """
-    def set_masses(self, new_masses: nuTens._pyNuTens.tensor.Tensor) -> None:
+    def set_masses(self, new_masses: nuTens._pyNuTens.tensor.Tensor) -> BaseMatterSolver:
         """
         Set the neutrino masses the solver should use
         """
-    def set_mixing_matrix(self, new_matrix: nuTens._pyNuTens.tensor.Tensor) -> None:
+    def set_mixing_matrix(self, new_matrix: nuTens._pyNuTens.tensor.Tensor) -> BaseMatterSolver:
         """
         Set the mixing matrix that the solver should use
         """
@@ -28,15 +27,27 @@ class BaseMixingMatrix:
     def build(self) -> nuTens._pyNuTens.tensor.Tensor:
         ...
 class ConstDensitySolver(BaseMatterSolver):
-    def __init__(self, n_generations: typing.SupportsInt | typing.SupportsIndex, density: typing.SupportsFloat | typing.SupportsIndex, anti_neutrino: bool = False) -> None:
+    def __init__(self, n_generations: typing.SupportsInt | typing.SupportsIndex) -> None:
         ...
     def get_density(self) -> float:
         """
         Get the density used by the solver
         """
-    def set_density(self, new_value: typing.SupportsFloat | typing.SupportsIndex) -> None:
+    def set_antineutrino(self, new_value: bool) -> ConstDensitySolver:
         """
         Set the density that the solver should use
+        """
+    def set_density(self, new_value: typing.SupportsFloat | typing.SupportsIndex) -> ConstDensitySolver:
+        """
+        Set the density that the solver should use
+        """
+    def set_masses(self, new_value: nuTens._pyNuTens.tensor.Tensor) -> ConstDensitySolver:
+        """
+        Set the neutrino masses that the solver should use
+        """
+    def set_mixing_matrix(self, new_value: nuTens._pyNuTens.tensor.Tensor) -> ConstDensitySolver:
+        """
+        Set the mixing that the solver should use
         """
 class DPpropagator(Propagator):
     def __init__(self, NR_iterations: typing.SupportsInt | typing.SupportsIndex) -> None:
@@ -69,7 +80,7 @@ class DPpropagator(Propagator):
         """
         set the density
         """
-    def set_energies(self, new_energies: nuTens._pyNuTens.tensor.Tensor) -> None:
+    def set_energies(self, new_energies: nuTens._pyNuTens.tensor.Tensor) -> DPpropagator:
         """
         set the neutrino energies
         """
@@ -84,18 +95,24 @@ class DPpropagator(Propagator):
 class PMNSmatrix(BaseMixingMatrix):
     def __init__(self) -> None:
         ...
-    def get_delta_cp_tensor(self) -> nuTens._pyNuTens.tensor.Tensor:
+    def get_deltacp_tensor(self) -> nuTens._pyNuTens.tensor.Tensor:
         ...
-    def get_theta_12_tensor(self) -> nuTens._pyNuTens.tensor.Tensor:
+    def get_theta12_tensor(self) -> nuTens._pyNuTens.tensor.Tensor:
         ...
-    def get_theta_13_tensor(self) -> nuTens._pyNuTens.tensor.Tensor:
+    def get_theta13_tensor(self) -> nuTens._pyNuTens.tensor.Tensor:
         ...
-    def get_theta_23_tensor(self) -> nuTens._pyNuTens.tensor.Tensor:
+    def get_theta23_tensor(self) -> nuTens._pyNuTens.tensor.Tensor:
         ...
-    def set_parameter_values(self, theta_12: typing.SupportsFloat | typing.SupportsIndex, theta_13: typing.SupportsFloat | typing.SupportsIndex, theta_23: typing.SupportsFloat | typing.SupportsIndex, delta_cp: typing.SupportsFloat | typing.SupportsIndex) -> None:
+    def set_deltacp(self, delta_cp: typing.SupportsFloat | typing.SupportsIndex) -> PMNSmatrix:
+        ...
+    def set_theta12(self, theta_12: typing.SupportsFloat | typing.SupportsIndex) -> PMNSmatrix:
+        ...
+    def set_theta13(self, theta_13: typing.SupportsFloat | typing.SupportsIndex) -> PMNSmatrix:
+        ...
+    def set_theta23(self, theta_23: typing.SupportsFloat | typing.SupportsIndex) -> PMNSmatrix:
         ...
 class Propagator:
-    def __init__(self, n_generations: typing.SupportsInt | typing.SupportsIndex, baseline: typing.SupportsFloat | typing.SupportsIndex, anti_neutrino: bool = False) -> None:
+    def __init__(self, n_generations: typing.SupportsInt | typing.SupportsIndex) -> None:
         ...
     def calculate_probabilities(self) -> nuTens._pyNuTens.tensor.Tensor:
         """
@@ -105,38 +122,27 @@ class Propagator:
         """
         Get the baseline used by the propagator
         """
-    def set_antineutrino(self, new_value: bool) -> None:
+    def set_antineutrino(self, new_value: bool) -> Propagator:
         """
         Set whether the propagator should calculate oscillations for anti-neutrinos
         """
-    def set_baseline(self, new_value: typing.SupportsFloat | typing.SupportsIndex) -> None:
+    def set_baseline(self, new_value: typing.SupportsFloat | typing.SupportsIndex) -> Propagator:
         """
         Set the baseline that the propagator should use
         """
-    def set_energies(self, new_energies: nuTens._pyNuTens.tensor.Tensor) -> None:
+    def set_energies(self, new_energies: nuTens._pyNuTens.tensor.Tensor) -> Propagator:
         """
         Set the neutrino energies that the propagator should use
         """
-    def set_masses(self, new_masses: nuTens._pyNuTens.tensor.Tensor) -> None:
+    def set_masses(self, new_masses: nuTens._pyNuTens.tensor.Tensor) -> Propagator:
         """
         Set the neutrino mass state eigenvalues
         """
-    def set_matter_solver(self, new_matter_solver: BaseMatterSolver) -> None:
+    def set_matter_solver(self, new_matter_solver: BaseMatterSolver) -> Propagator:
         """
         Set the matter effect solver that the propagator should use
         """
-    @typing.overload
-    def set_mixing_matrix(self, new_matrix: nuTens._pyNuTens.tensor.Tensor) -> None:
-        """
-        Set the mixing matrix that the propagator should use
-        """
-    @typing.overload
-    def set_mixing_matrix(self, indices: collections.abc.Sequence[typing.SupportsInt | typing.SupportsIndex], value: typing.SupportsFloat | typing.SupportsIndex) -> None:
-        """
-        Set a particular value within the mixing matrix used by the propagator
-        """
-    @typing.overload
-    def set_mixing_matrix(self, indices: collections.abc.Sequence[typing.SupportsInt | typing.SupportsIndex], value: typing.SupportsComplex | typing.SupportsFloat | typing.SupportsIndex) -> None:
+    def set_mixing_matrix(self, new_matrix: nuTens._pyNuTens.tensor.Tensor) -> Propagator:
         """
         Set the mixing matrix that the propagator should use
         """
