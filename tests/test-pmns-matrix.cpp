884b309507da5872f85644f785aacec49024b1e0
Ewan W Miller <e.w.miller@imperial.ac.uk>
2026-06-18 17:26:15 +0100

    restructure tests to have header and cpp file each

--- tests/test-pmns-matrix.cpp
+++ tests/test-pmns-matrix.cpp
@@ -1,9 +1,4 @@
-#include <gtest/gtest.h> // NOLINT
-// alias the gtest "testing" namespace
-namespace gtest = ::testing;
-
-#include <iostream>
-#include <nuTens/propagator/pmns-matrix.hpp>
+#include <tests/test-pmns-matrix.hpp>
 
 // magic numbers are fine for testing!
 // NOLINTBEGIN(readability-magic-numbers, cppcoreguidelines-avoid-magic-numbers)
---
+++ tests/test-pmns-matrix.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <gtest/gtest.h> // NOLINT
+// alias the gtest "testing" namespace
+namespace gtest = ::testing;
+
+#include <iostream>
+#include <nuTens/propagator/pmns-matrix.hpp>
