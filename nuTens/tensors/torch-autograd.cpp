e7bba1ce6bf1d04915952c71ba232dc481f6e540
Ewan Miller <ewan.miller@kcl.ac.uk>
2026-08-05 13:40:13 +0100

    add check that the leaf tensor has requiresGrad

--- nuTens/tensors/torch-autograd.cpp
+++ nuTens/tensors/torch-autograd.cpp
@@ -7,6 +7,11 @@ nuTens::Tensor grad(const nuTens::Tensor &final, const nuTens::Tensor &leaf)
 {
     NT_PROFILE();
 
+    if (!leaf.getRequiresGrad())
+    {
+        throw std::invalid_argument("Leaf tensor must have requiresGrad == true");
+    }
+
     return nuTens::Tensor::fromTorchTensor(torch::autograd::grad(
         {final.getTensor()}, {leaf.getTensor()}, /*grad_outputs=*/{}, /*retain_graph=*/true, /*create_graph=*/true)[0]);
 }
