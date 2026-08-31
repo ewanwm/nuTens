a625e4c0b32ed29e95be9e326c8814d93943fbc9
Ewan W Miller <e.w.miller@imperial.ac.uk>
2026-08-31 17:32:12 +0100

    lint and format cmake files

--- cmake/nuTens-dependencies.cmake
+++ cmake/nuTens-dependencies.cmake
@@ -1,68 +1,74 @@
-## define dependencies of nutens which will be included using cpm where possible
+# define dependencies of nutens which will be included using cpm where possible
 
-## ==== Pytorch ====
+# ==== Pytorch ====
 if(NT_USE_TORCH)
-    find_package(Torch)
+  find_package(Torch)
 
-    if(Torch_FOUND)
-        message(STATUS "Found pre-installed Torch at ${Torch_DIR}")
-    
-    elseif(NT_TORCH_FROM_PIP)
-        
-        message(STATUS "Torch not found - installing with pip")
-        
-        ## check if we are in a virtual environment before pip installing
-        if(DEFINED ENV{VIRTUAL_ENV} OR DEFINED ENV{CONDA_PREFIX})
-            set(_pip_args)
- 
-        else()
-            ## we're not in a virtual env, make sure the user is really sure that's ok
-            if(NT_ALLOW_GLOBAL_PYTHON_ENV)
-                set(_pip_args "--user")
-            else()
-                message(FATAL_ERROR "\
+  if(Torch_FOUND)
+    message(STATUS "Found pre-installed Torch at ${Torch_DIR}")
+
+  elseif(NT_TORCH_FROM_PIP)
+
+    message(STATUS "Torch not found - installing with pip")
+
+    # check if we are in a virtual environment before pip installing
+    if(DEFINED ENV{VIRTUAL_ENV} OR DEFINED ENV{CONDA_PREFIX})
+      set(_pip_args)
+
+    else()
+      # we're not in a virtual env, make sure the user is really sure that's ok
+      if(NT_ALLOW_GLOBAL_PYTHON_ENV)
+        set(_pip_args "--user")
+      else()
+        message(
+          FATAL_ERROR
+            "\
 You're not in a python virtual environment but trying to install pytorch! \n \
 This is ill advised, but if you're sure that's what you want to do you can set the option \n \
   -D NT_ALLOW_GLOBAL_PYTHON_ENV")
 
-            endif() ## end of if(NT_ALLOW_GLOBAL_PYTHON_ENV)
+      endif() # end of if(NT_ALLOW_GLOBAL_PYTHON_ENV)
 
-        endif() ## done checking for virtual env 
-        
-        find_package(Python COMPONENTS Interpreter REQUIRED)
-        
-        ## install torch Python package using pip
-        execute_process(COMMAND ${Python_EXECUTABLE} -m pip install -r ${PROJECT_SOURCE_DIR}/PyTorch_requirements.txt)
-        
-        ## need to do some absolute tomfoolery to get the path to the torch shared library
-        execute_process(
-            COMMAND ${Python_EXECUTABLE} ${PROJECT_SOURCE_DIR}/cmake/torch-cmake-prefix.py
-            OUTPUT_VARIABLE _TORCH_CMAKE_PREFIX
-        )
-        string(REPLACE ' "" TORCH_CMAKE_PREFIX ${_TORCH_CMAKE_PREFIX})
+    endif() # done checking for virtual env
 
-        message(STATUS "TORCH CMAKE PREFIX: ${TORCH_CMAKE_PREFIX}")
-        list(APPEND CMAKE_PREFIX_PATH ${TORCH_CMAKE_PREFIX})
-    
-    elseif(NT_TORCH_FROM_SCRATCH)
+    find_package(
+      Python
+      COMPONENTS Interpreter
+      REQUIRED)
 
-        CPMAddPackage(
-            NAME Torch
-            GIT_REPOSITORY git@github.com:pytorch/pytorch.git
-            VERSION 2.7.1
-        )
+    # install torch Python package using pip
+    execute_process(COMMAND ${Python_EXECUTABLE} -m pip install -r
+                            ${PROJECT_SOURCE_DIR}/PyTorch_requirements.txt)
 
-    else()
-        message(FATAL_ERROR "\
+    # need to do some absolute tomfoolery to get the path to the torch shared
+    # library
+    execute_process(
+      COMMAND ${Python_EXECUTABLE}
+              ${PROJECT_SOURCE_DIR}/cmake/torch-cmake-prefix.py
+      OUTPUT_VARIABLE _TORCH_CMAKE_PREFIX)
+    string(REPLACE ' "" TORCH_CMAKE_PREFIX ${_TORCH_CMAKE_PREFIX})
+
+    message(STATUS "TORCH CMAKE PREFIX: ${TORCH_CMAKE_PREFIX}")
+    list(APPEND CMAKE_PREFIX_PATH ${TORCH_CMAKE_PREFIX})
+
+  elseif(NT_TORCH_FROM_SCRATCH)
+
+    cpmaddpackage(NAME Torch GIT_REPOSITORY git@github.com:pytorch/pytorch.git
+                  VERSION 2.7.1)
+
+  else()
+    message(
+      FATAL_ERROR
+        "\
 Torch not found and no method specified to install it. \n \
 either go install torch and try again or set one of\n \
     -DNT_TORCH_FROM_PIP=ON\n \
-    -DNT_TORCH_FROM_SCRATCH=ON 
+    -DNT_TORCH_FROM_SCRATCH=ON
 ")
 
-    endif() ## end of if(Torch_FOUND)
+  endif() # end of if(Torch_FOUND)
 
-endif() ## end of if(NT_USE_TORCH)
+endif() # end of if(NT_USE_TORCH)
 
 message("CMAKE_PREFIX_PATH: ${CMAKE_PREFIX_PATH}")
 execute_process(COMMAND ls ${CMAKE_PREFIX_PATH})
@@ -73,50 +79,41 @@ message(STATUS "Torch libs ${TORCH_LIBS}")
 message("Torch cxx flags: ${TORCH_CXX_FLAGS}")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${TORCH_CXX_FLAGS}")
 
-## ==== spdlog ====
-## must be AddPackage not FindPackage as it needs to be compiled as part of nuTens
-## not externally
-CPMAddPackage(
-    NAME spdlog
-    GITHUB_REPOSITORY gabime/spdlog
-    VERSION 1.17.0
-)
+# ==== spdlog ====
+# must be AddPackage not FindPackage as it needs to be compiled as part of
+# nuTens not externally
+cpmaddpackage(NAME spdlog GITHUB_REPOSITORY gabime/spdlog VERSION 1.17.0)
 
 # ==== google tests ====
 if(NT_COMPILE_TESTS)
-    CPMFindPackage(
-        NAME googletest
-        GITHUB_REPOSITORY "google/googletest"
-        VERSION 1.17.0
-        OPTIONS 
-        "BUILD_GMOCK OFF"
-    )
+  cpmfindpackage(
+    NAME
+    googletest
+    GITHUB_REPOSITORY
+    "google/googletest"
+    VERSION
+    1.17.0
+    OPTIONS
+    "BUILD_GMOCK OFF")
 endif()
 
 # ==== google benchmark ====
 if(NT_ENABLE_BENCHMARKING)
-    message("Enabling benchmarking")
-    CPMAddPackage(
-        GITHUB_REPOSITORY "google/benchmark"
-        VERSION 1.8.5 
-        OPTIONS 
-        "BENCHMARK_ENABLE_TESTING OFF"
-    )
+  message("Enabling benchmarking")
+  cpmaddpackage(GITHUB_REPOSITORY "google/benchmark" VERSION 1.8.5 OPTIONS
+                "BENCHMARK_ENABLE_TESTING OFF")
 else()
-    message("Won't benchmark")
+  message("Won't benchmark")
 endif()
 
 # ==== pybind11 ====
 if(NT_ENABLE_PYTHON)
-    message("Enabling python")
-    
-    find_package(Python COMPONENTS Interpreter Development)
+  message("Enabling python")
 
-    CPMAddPackage(
-        GITHUB_REPOSITORY "pybind/pybind11"
-        VERSION 3.0.4 
-    )
+  find_package(Python COMPONENTS Interpreter Development)
+
+  cpmaddpackage(GITHUB_REPOSITORY "pybind/pybind11" VERSION 3.0.4)
 
 else()
-    message("Won't enable python interface")
+  message("Won't enable python interface")
 endif()
--- cmake/nuTens-options.cmake
+++ cmake/nuTens-options.cmake
@@ -1,151 +1,111 @@
-############################
-#### Basic user options ####
-############################
+# ##############################################################################
+# Basic user options ####
+# ##############################################################################
+
+option(NT_USE_TORCH "Use torch as the backend for dealing with tensors" ON)
+
+option(NT_TORCH_FROM_PIP
+       "If it is not found, torch will be installed using pip" ON)
+
+option(NT_ALLOW_GLOBAL_PYTHON_ENV
+       "Allow installing pip packages in global python environment" OFF)
+
+option(NT_ENABLE_PYTHON "enable python interface" OFF)
+
+option(NT_COMPILE_TESTS "whether or not to compile unit and integration tests"
+       ON)
+
+# ##############################################################################
+# advanced user options ####
+# ##############################################################################
 
 option(
-    NT_USE_TORCH 
-    "Use torch as the backend for dealing with tensors"
-    ON
-)
-
-option(
-    NT_TORCH_FROM_PIP 
-    "If it is not found, torch will be installed using pip"
-    ON
-)
-
-option(
-    NT_ALLOW_GLOBAL_PYTHON_ENV 
-    "Allow installing pip packages in global python environment"
-    OFF
-)
-
-option(
-    NT_ENABLE_PYTHON 
-    "enable python interface"
-    OFF
-)
-
-option(
-    NT_COMPILE_TESTS 
-    "whether or not to compile unit and integration tests"
-    ON
-)
-
-###############################
-#### advanced user options ####
-###############################
-
-option(
-    NT_TORCH_FROM_SCRATCH 
-    "If it is not found, torch will be compiled from scratch using CPM (very slow but maybe useful for debugging builds)"
-    OFF
-)
+  NT_TORCH_FROM_SCRATCH
+  "If it is not found, torch will be compiled from scratch using CPM (very slow but maybe useful for debugging builds)"
+  OFF)
 mark_as_advanced(NT_TORCH_FROM_SCRATCH)
 
-option(
-    NT_ENABLE_BENCHMARKING 
-    "enable benchmarking using google benchmark"
-    OFF
-)
+option(NT_ENABLE_BENCHMARKING "enable benchmarking using google benchmark" OFF)
 mark_as_advanced(NT_ENABLE_BENCHMARKING)
 
-option(
-    NT_ENABLE_GPU_BENCHMARKING 
-    "enable benchmarking on GPU"
-    OFF
-)
+option(NT_ENABLE_GPU_BENCHMARKING "enable benchmarking on GPU" OFF)
 mark_as_advanced(NT_ENABLE_GPU_BENCHMARKING)
 
 option(
-    NT_COMPILE_GPU_TESTS 
-    "whether or not to compile unit and integration tests for GPU bound tensors"
-    ON
-)
+  NT_COMPILE_GPU_TESTS
+  "whether or not to compile unit and integration tests for GPU bound tensors"
+  ON)
 mark_as_advanced(NT_COMPILE_GPU_TESTS)
 
-option(
-    NT_TEST_COVERAGE 
-    "produce code coverage reports when running tests"
-    OFF
-)
+option(NT_TEST_COVERAGE "produce code coverage reports when running tests" OFF)
 mark_as_advanced(NT_TEST_COVERAGE)
 
-option(
-    NT_BUILD_TIMING 
-    "output time to build each target"
-    OFF
-)
+option(NT_BUILD_TIMING "output time to build each target" OFF)
 mark_as_advanced(NT_BUILD_TIMING)
 
-option(
-    NT_USE_PCH 
-    "NT_USE_PCH"
-    OFF
-)
+option(NT_USE_PCH "NT_USE_PCH" OFF)
 mark_as_advanced(NT_USE_PCH)
 
-option(
-    BUILD_SHARED_LIBS 
-    "Build using shared libs"
-    ON
-)
+option(BUILD_SHARED_LIBS "Build using shared libs" ON)
 mark_as_advanced(BUILD_SHARED_LIBS)
 
-###################################################################
-#### Apply flags and other global vars implied by user options ####
-###################################################################
+# ##############################################################################
+# Apply flags and other global vars implied by user options ####
+# ##############################################################################
 
-## to build the python library we require to build with the pic flag
+# to build the python library we require to build with the pic flag
 if(NT_ENABLE_PYTHON)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC")
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fPIC")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC")
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fPIC")
 
-    ## for now we also need to build with static libs
-    message(WARNING "BUILD_SHARED_LIBS cannot be used when building python interface, setting BUILD_SHARED_LIBS=OFF")
-    set(BUILD_SHARED_LIBS OFF)
-    set(SPDLOG_BUILD_SHARED OFF)
+  # for now we also need to build with static libs
+  message(
+    WARNING
+      "BUILD_SHARED_LIBS cannot be used when building python interface, setting BUILD_SHARED_LIBS=OFF"
+  )
+  set(BUILD_SHARED_LIBS OFF)
+  set(SPDLOG_BUILD_SHARED OFF)
 endif()
 
-# Need to add some special compile flags to check the code test coverage 
+# Need to add some special compile flags to check the code test coverage
 if(NT_TEST_COVERAGE)
-    message("Adding flags to check test coverage")
-    add_compile_options("-O0")
-    add_compile_options("--coverage")
-    add_link_options("--coverage")
+  message("Adding flags to check test coverage")
+  add_compile_options("-O0")
+  add_compile_options("--coverage")
+  add_link_options("--coverage")
 else()
-    message("Won't check test coverage")
+  message("Won't check test coverage")
 endif()
 
 # enable ctest
 if(NT_COMPILE_TESTS)
-    message("Compiling tests")
-    enable_testing()
+  message("Compiling tests")
+  enable_testing()
 else()
-    message("Won't compile tests")
+  message("Won't compile tests")
 endif()
 
 # have this optional as it's not supported on all CMake platforms
 if(NT_BUILD_TIMING)
-    set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE "${CMAKE_COMMAND} -E time")
+  set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE "${CMAKE_COMMAND} -E time")
 endif()
 
-
-###########################################
-#### Print out user options and values ####
-###########################################
+# ##############################################################################
+# Print out user options and values ####
+# ##############################################################################
 
 # Print out a handy message to more easily see the config options
-message( STATUS "The following variables have been used to configure the build: " )
-get_cmake_property(_variableNames VARIABLES)
-list (SORT _variableNames)
-foreach (_variableName ${_variableNames})
-    unset(MATCHED)
-    string(REGEX MATCH "^NT_*" MATCHED ${_variableName})
-    if (NOT MATCHED)
-        continue()
-    endif()
-    
-    message(STATUS "  ${_variableName}=${${_variableName}}")
+message(
+  STATUS "The following variables have been used to configure the build: ")
+get_cmake_property(variable_names VARIABLES)
+list(SORT variable_names)
+foreach(variable_name ${variable_names})
+  unset(MATCHED)
+  string(REGEX MATCH "^NT_*" MATCHED ${variable_name})
+  if(NOT MATCHED)
+    continue()
+  endif()
+
+  message(STATUS "  ${_variableName}=${${_variableName}}")
 endforeach()
 message(STATUS "  BUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}")
